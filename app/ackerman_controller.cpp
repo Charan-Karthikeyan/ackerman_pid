a098026cf00e5b8c7b72ea870486777dea6f838b
charan <pvcharankarthikeyan@gmail.com>
2019-10-12 18:38:12 -0400

    The function definition for the ackerman_controller class

---
+++ app/ackerman_controller.cpp
@@ -0,0 +1,27 @@
+/**
+ * @file ackerman_controller.cpp
+ * @author Ashwin Varghese Kuruttukulam
+ * @author Charan Karthikeyan
+ * @brief Defines the function to calculate the Radius, wheel velocity
+ * and update the current heading.
+ * @Copyright "Copyright 2019 <Ashwin Varghese Kuruttukulam>
+ * @Copyright "Copyright 2019 <Charan Karthikeyan>
+ */
+
+#include "ackerman_controller.hpp"
+
+void ackerman_controller::calcRadius(){
+
+}
+
+void ackerman_controller::calcWheelVel(){
+
+}
+
+double ackerman_controller::compute(double headingFeedback , double &rWheelVel, double &lWheelVel){
+
+}
+
+void ackerman_controller::changeSetPoints(double headingsp,double velocitysp){
+
+}
