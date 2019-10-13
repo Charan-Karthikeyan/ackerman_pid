62f4725b1d7f39c8fae3665bb61f6822aef7201f
Ashwin Varghese Kuruttukulam <ashwinvk94@gmail.com>
2019-10-13 11:28:31 -0400

    Add initial test for sim

---
+++ test/test_ackerman_sim.cpp
@@ -0,0 +1,10 @@
+#include <gtest/gtest.h>
+
+#include "ackerman_sim.hpp"
+
+TEST(VaidateAckermanController,TestCompute) {
+  ackerman_sim ack_sim;
+  //TODO calculate the value of and add to the EQ from the cpp file.
+  EXPECT_EQ(ack_sim.compute(10),);
+
+}
