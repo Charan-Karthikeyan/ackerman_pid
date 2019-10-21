4c5385191a0ec91bed7733715b9a259112bdceeb
charan <pvcharankarthikeyan@gmail.com>
2019-10-21 09:45:59 -0400

    Added the copywrite comment and checked the code. Task2.6

--- test/main.cpp
+++ test/main.cpp
@@ -1,3 +1,12 @@
+/**
+ * @file main.cpp
+ * @author Ashwin Varghese Kuruttukulam
+ * @author Charan Karthikeyan
+ * @brief Main program that runs the tests for the pid, ackerman controller and the ackerman simulation classes.
+ *
+ * @Copyright "Copyright 2019 <Ashwin Varghese Kuruttukulam>
+ * @Copyright "Copyright 2019 <Charan Karthikeyan>
+ */
 #include <gtest/gtest.h>
 
 int main(int argc, char** argv) {
